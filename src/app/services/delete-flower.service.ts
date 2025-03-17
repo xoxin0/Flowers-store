4f85074fcdcd42ccb4f340bf5964584adc0d19c2
xoxin <xoxinweb@gmail.com>
2025-03-17 15:47:27 +0300

    didn't see

---
+++ src/app/services/delete-flower.service.ts
@@ -0,0 +1,25 @@
+import {
+  inject,
+  Injectable
+} from '@angular/core';
+
+import { Flower } from '../interfaces/flower';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DeleteFlowerService {
+
+  private flowersUrl = 'http://localhost:3000/api/flowers/flower';
+
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  public deleteFlowerInDataBase(id: number): Observable<Flower> {
+    return this.http.delete<Flower>(`${this.flowersUrl}/${id}`);
+  }
+}
